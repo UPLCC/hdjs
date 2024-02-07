a1a04a9056f95f2902266b533527d94c1d0004b1
UPLCC <143597013+UPLCC@users.noreply.github.com>
2024-02-07 11:10:16 +0800

    Create index.js
    .

---
+++ index.js
@@ -0,0 +1,14 @@
+function xyy(){
+ymb.style.display="block";
+syya.style.display="block";
+a.style.animationName="a";
+a.style.animationDuration="0.5s";
+a.style.width="100vw";
+}
+function syy(){
+ymb.style.display="none";
+syya.style.display="none";
+a.style.animationName="b";
+a.style.animationDuration="0.5s";
+a.style.width="0px";
+}
